b35b24f4c5f9f0a098107cd287c4532cb7d695ea
Rennie <7017622+Renniej@users.noreply.github.com>
2025-04-23 16:35:28 -0400

    add game modes for being a guesser or encoder
    The game can now be played with a single player
    who can decide if they or the computer should
    guess codes.

--- app.rb
+++ app.rb
@@ -1,9 +1,33 @@
 require_relative "mastermind"
 require_relative "player"
+require_relative "computer"
+
+def input_player
+  puts "What's your name?"
+  Player.new(gets.chomp)
+end
+
+def input_game_mode 
+  loop do
+   puts "Would you like to be 'guesser' or 'encoder'"
+   game_mode = gets.chomp.downcase
+   return game_mode if (game_mode == "guesser" || game_mode == "encoder") 
+   puts "Incorrect option"
+  end
+end
+
+def get_players() 
+  player = input_player()
+  game_mode = input_game_mode
+  return [Computer.new, player] if game_mode == "guesser"
+  return [player, Computer.new] if game_mode == "encoder"
+end
+
+  
+
 
 def main 
-  player_1 = Player.new("Jake")
-  player_2 = Player.new("George")
+  player_1, player_2 = get_players
   game = Game.new(player_1, player_2)
   game.start
 end
