0a609172bbfb5f56658a1b75f889a154bbf798cb
Rennie <7017622+Renniej@users.noreply.github.com>
2025-04-22 20:30:02 -0400

    add start function to game
    used to initiate a match between player 1 & 2

--- mastermind.rb
+++ mastermind.rb
@@ -21,7 +21,7 @@
 #   create variable called list_of_feedback with initial value of []
 #   for each number in guess compare
 #       if the number exist in the array and guess[i] == code[i]
-#           push CORRECT_RIGHT_POSITON into list_of_feedback
+#           push CORRECT_RIGHT_POSITION into list_of_feedback
 #       else if number exist in the array but guess[i] != code[i]
 #           push CORRECT_WRONG_POSITION into the array
 #       end
@@ -54,7 +54,7 @@
 
 FEEDBACK = {
   CORRECT_WRONG_POSITION: "Correct selection, wrong position",
-  CORRECT_RIGHT_POSITON: "Correct selection, correct position",
+  CORRECT_RIGHT_POSITION: "Correct selection, correct position",
   EMPTY_FEEDBACK: "Seems like you got everythin wrong..."
 }
 
@@ -70,11 +70,55 @@ class Game
   def get_feedback(guesses)
    guesses.each_with_index.map do |guess, index|
       if @code.includes? guess 
-        @code[index] == guess ? FEEDBACK.CORRECT_RIGHT_POSITON : FEEDBACK.CORRECT_WRONG_POSITION
+        @code[index] == guess ? FEEDBACK.CORRECT_RIGHT_POSITION : FEEDBACK.CORRECT_WRONG_POSITION
       end
     end.compact.shuffle
   end
 
 
+  # start()
+#   output player1.name. pick a code (have input be blocked with ****)
+#   game.code = player1.input().split split into an array of 4 numbers
+#   Loop 12 times
+#     output player2.name, please guess a color
+#     create variable of type array named guess with the initial values of player2.inputColors().split() into array of 4 numbers
+#     create variable called feedback =  get_feedback(guess)
+#     
+#     if feedback is the size of 4 and each element is of value CORRECT_RIGHT_POSITION
+#       output player1.name WON!
+#     else if 12th loop
+#       output "Out of attempts.. player_2.name wins!"
+#     else if feedback is empty
+#       output "seems like you got everything wrong...."
+#     else 
+#       output feedback
+#      end
+#    end   
 
+  def start 
+    puts "#{player1.name} pick a code"
+    @code = player1.input.split.map(&:to_i)
+     
+    12.times do |attempt|
+      puts "#{player2.name} please guess the 4 color code"
+      guesses = player2.input.split.map(&:to_i)
+      feedback = get_feedback(guesses)
+      player2_won =  feedback.all? {& == FEEDBACK.CORRECT_RIGHT_POSITION} && feedback.size == 
+      
+      if player2_won 
+        puts "#{player2.name} won!"
+        break;
+      elsif feedback.empty? 
+        puts "#{player2.name} got everything wrong..."
+      elsif attempt == 12  
+         puts "Out of attempts.. #{player1.name} wins!"
+      else
+        puts feedback
+      end
+        
+
+   
+    end
+    
+  end
 end
