d939808209de11c82706dcec3254d22d8df69603
Rennie <7017622+Renniej@users.noreply.github.com>
2025-04-23 16:55:35 -0400

    allow computer players to be aware of game state
    The computer needs knowledge of the game's state
    in order to determine how accurate it's guess was.
    
    To promote proper seperation of concerns I wanted
    the computer to just read the STDOUT to determine
    how accurate it's guess is. (Essentially mimicing
    a real player reading the screen) However this
    proved unviable. Thus the computer now references
    the game object directly.

--- computer.rb
+++ computer.rb
@@ -2,11 +2,19 @@ require_relative "player"
 
 class Computer < Player
 
+  attr_accessor :game
+
   def initialize()
     super("Computer")
+    @game = nil
     @choices = (1000..9999).select { |num| num.to_s.chars.uniq.length == 4 }
   end
   
+  
+
+  def get_guess
+    
+  end
 
   def get_input
     code = @choices.delete(@choices.sample).to_s
