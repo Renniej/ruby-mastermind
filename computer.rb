c748973d35057fbd24bccdb857baacf2999d7c61
Rennie <7017622+Renniej@users.noreply.github.com>
2025-04-23 16:55:35 -0400

    allow computer players to be aware of game state
    The computer needs knowledge of the game's state
    in order to determine how accurate it's guess was.
    
    To promote proper seperation of concerns I wanted
    the computer to just read the STDOUT to determine
    how accurate it's guess is. (Essentially mimicing
    a real player reading the screen) However this
    proved unviable. Thus the computer now references
    the game object directly.

--- computer.rb
+++ computer.rb
@@ -2,14 +2,25 @@ require_relative "player"
 
 class Computer < Player
 
+  attr_accessor :game
+
   def initialize()
     super("Computer")
+    @game = nil
     @choices = (1000..9999).select { |num| num.to_s.chars.uniq.length == 4 }
   end
   
 
+  def get_guess
+    @choices.delete(@choices.sample).to_s
+  end
+
+  def is_guesser? 
+    game.is_guesser?(self)
+  end
+  
   def get_input
-    code = @choices.delete(@choices.sample).to_s
+    code = if is_guesser? then get_guess else @choices.delete(@choices.sample).to_s end
     puts code
     code
   end
--- mastermind.rb
+++ mastermind.rb
@@ -1,56 +1,4 @@
-#Player 1 picks a random set of colors (secret code)
-# Loop 12 times
-#   Player 2 submits a guess 
-#   If guess is correct end the game and break the loop
-#   Player 1 submits feedback
-#   
-# Player(name)
-#   input() : number
-#       gets.to_i
-#   
-#
-# ENUM Feedback 
-#   CORRECT_WRONG_POSITION = "Correct number, wrong position"
-#   CORRECT_RIGHT_POSITION = "Correct number, correct position"
-#   
-#game(player1, player2)
-#
-
-#     
-# get_feedback(guess : number[])
-#   create variable called list_of_feedback with initial value of []
-#   for each number in guess compare
-#       if the number exist in the array and guess[i] == code[i]
-#           push CORRECT_RIGHT_POSITION into list_of_feedback
-#       else if number exist in the array but guess[i] != code[i]
-#           push CORRECT_WRONG_POSITION into the array
-#       end
-#    end
-#    return list_of_feedback.randomize the order
-#  end
-#  
-#
-# start()
-#   output player1.name. pick a code (have input be blocked with ****)
-#   game.code = player1.input().split split into an array of 4 numbers
-#   Loop 12 times
-#     output player2.name, please guess a color
-#     create variable of type array named guess with the initial values of player2.inputColors().split() into array of 4 numbers
-#     create variable called feedback =  get_feedback(guess)
-#     
-#     if feedback is the size of 4 and each element is of value CORRECT_RIGHT_POSITION
-#       output player1.name WON!
-#     else if 12th loop
-#       output "Out of attempts.. player_2.name wins!"
-#     else if feedback is empty
-#       output "seems like you got everything wrong...."
-#     else 
-#       output feedback
-#      end
-#    end   
-#
-#     
-#     
+require_relative "computer"
 
 FEEDBACK = {
   CORRECT_WRONG_POSITION: "Correct selection, wrong position",
@@ -59,11 +7,16 @@ FEEDBACK = {
 }
 
 class Game
+  attr_reader :list_of_feedback
 
   def initialize(player1, player2)
     @code = []
+    @list_of_feedback = []
     @player1 = player1
     @player2 = player2
+    
+    player1.game = self if (player1.is_a Computer) 
+    player2.game = self if (player2.is_a Computer) 
   end
 
   def start 
@@ -101,6 +54,10 @@ class Game
     { valid: true, error_message: "" }
   end
 
+  def is_guesser?(player)
+    player == @player2
+  end
+
   def input_code(player)
     loop do
       input = player.get_input.chars.map(&:to_i)
@@ -115,9 +72,9 @@ class Game
   def run_round(player) #run round returns true or false based on if the player correct guessed the code
     puts "#{player.name} please guess the color code"
     guess = input_code(player)
-    list_of_feedback = get_feedback(guess)
-    display_feedback(list_of_feedback) 
-    list_of_feedback.all? {|feedback| feedback == FEEDBACK[:CORRECT_RIGHT_POSITION]} && list_of_feedback.size == 4
+    @list_of_feedback = get_feedback(guess)
+    display_feedback(@list_of_feedback) 
+    @list_of_feedback.all? {|feedback| feedback == FEEDBACK[:CORRECT_RIGHT_POSITION]} && list_of_feedback.size == 4
   end
 
 
